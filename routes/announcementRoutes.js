badd354ad66e9dead28ab254c2dbb487612c4464
Shubham Chandwani <chandwanishubham@gmail.com>
2023-06-19 19:59:07 +0530

    Added Announcement Api's

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { createAnnouncement, getAnnouncements, announcementPostComments } = require('../controllers/announcementController');
+const { createAnnouncement, getAnnouncements, announcementPostComments,getAnnouncementComments,editAnnouncement,deleteAnnouncement } = require('../controllers/announcementController');
 const { isAuth } = require('../middleware/isAuth');
 const { uploadToS3 } = require("../utils/s3");
 
@@ -12,6 +12,16 @@ router.post('/createAnnouncement', isAuth, uploadToS3, createAnnouncement);
 // get All Announcements
 router.get('/getAnnouncements/:classId', isAuth, getAnnouncements);
 
-router.post('/announcementpostcomment', isAuth, announcementPostComments);
+// Edit Announcement
+router.post('/editAnnouncement/:classId/:announcementId',isAuth,uploadToS3,editAnnouncement)
+
+// Delete Announcement 
+router.delete('/deleteAnnouncement/:classId/:announcementId',isAuth,deleteAnnouncement);
+
+// Get All comments for an Announcement
+router.get('/announcementGetComments/:announcementId',isAuth,getAnnouncementComments);
+
+// comment on a particular announcement
+router.post('/announcementPostComment/:announcementId', isAuth, announcementPostComments);
 
 module.exports = router;
